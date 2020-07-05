028464472cdc5e6bb2e3c9d358cb9bba5f46fb43
ahmed1amen <47221870+ahmed1amen@users.noreply.github.com>
2020-07-05 19:55:27 +0200

    css datatable

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -1,3 +1,3 @@
 <!-- DataTable Bootstrap -->
 
-<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
