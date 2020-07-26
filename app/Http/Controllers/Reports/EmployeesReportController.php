4c380adf390defc0147468f507341deac55534a1
ahmedtofaha10 <ahmedtofaha999@gmail.com>
2020-07-26 16:18:54 +0200

    filter employees report

--- app/Http/Controllers/Reports/EmployeesReportController.php
+++ app/Http/Controllers/Reports/EmployeesReportController.php
@@ -27,8 +27,8 @@ class EmployeesReportController extends AppBaseController
         $employeechart->dataset('الموظفين', 'bar',[16000, 20000,30000,40000,50000,10000,30000,40000,70000,60000,65000,85000])->backgroundcolor("rgb(236, 201, 75)")->color("rgb(236, 201, 75)");
 
         //
-        $from = $_GET['from'] == null ? '1970/12/12' : $_GET['from'] ;
-        $to   = $_GET['to']   == null ? '3000/12/12' : $_GET['to']   ;
+        $from = ($_GET['from'] ?? null) == null ? '1970/12/12' : $_GET['from'] ;
+        $to   = ($_GET['to'] ?? null)   == null ? '3000/12/12' : $_GET['to']   ;
         $market  = $_GET['marketID'] ?? null;
         $employeesTable = Employee::all()->where('created_at','>=',$from)
             ->where('created_at','<=',$to);
