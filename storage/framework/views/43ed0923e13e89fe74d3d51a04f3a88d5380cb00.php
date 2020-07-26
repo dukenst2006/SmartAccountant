1b2f42c5a90a31b3e8f7d19a928ae8631ef01684
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2020-07-26 17:16:47 +0200

    #comment remove view compailed files #type REMOVE

--- storage/framework/views/43ed0923e13e89fe74d3d51a04f3a88d5380cb00.php
+++
@@ -1,2 +0,0 @@
-(function(window,$){window.LaravelDataTables=window.LaravelDataTables||{};window.LaravelDataTables["%1$s"]=$("#%1$s").DataTable(%2$s);})(window,jQuery);
-<?php /**PATH /var/www/smart-acc.io/resources/views/vendor/datatables/script.blade.php ENDPATH**/ ?>
