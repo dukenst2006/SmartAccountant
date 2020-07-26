321554c3ecddd16700d5961ca75cf50dbf49e8a0
ahmedtofaha10 <ahmedtofaha999@gmail.com>
2020-07-26 15:45:52 +0200

    filter employees report

--- app/Http/Controllers/Reports/EmployeesReportController.php
+++ app/Http/Controllers/Reports/EmployeesReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Reports;
 use App\Charts\EmployeeChart;
 use App\Http\Controllers\AppBaseController;
+use App\Models\Employee;
 use App\Repositories\EmployeeRepository;
 
 class EmployeesReportController extends AppBaseController
@@ -25,9 +26,18 @@ class EmployeesReportController extends AppBaseController
         $employeechart->labels($employees->pluck('user.Name'));
         $employeechart->dataset('الموظفين', 'bar',[16000, 20000,30000,40000,50000,10000,30000,40000,70000,60000,65000,85000])->backgroundcolor("rgb(236, 201, 75)")->color("rgb(236, 201, 75)");
 
-
-
-        return view('admin.Reports.employees',compact('employeechart'));
+        //
+        $from = $_GET['from'] == null ? '1970/12/12' : $_GET['from'] ;
+        $to   = $_GET['to']   == null ? '3000/12/12' : $_GET['to']   ;
+        $market  = $_GET['marketID'] ?? null;
+        $employeesTable = Employee::all()->where('created_at','>=',$from)
+            ->where('created_at','<=',$to);
+        if ($market > 0){
+            $employeesTable = Employee::all()->where('created_at','>=',$from)
+                ->where('created_at','<=',$to)
+                ->where('MarketPlaceID','=',$market);
+        }
+        return view('admin.Reports.employees',compact('employeechart','employeesTable'));
     }
 
 }
