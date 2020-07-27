0fd481e7521e6bdc2ce9e16b4861a86e36c9eed5
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2020-07-27 14:30:30 +0200

    #comment Add Tax number with check if it enable or not #type MOD #time 00:15 M

---
+++ storage/framework/views/d786733c786c1d2d637f28efabb9a97293885431.php
@@ -0,0 +1,10 @@
+
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+        <b>Version</b> 1.0.0
+    </div>
+    <strong> حقوق الملكية محفوظة ل  © 2020 <a href="">Const Tech</a></strong>
+
+
+</footer>
+<?php /**PATH /var/www/smart-acc.io/resources/views/vendor/adminlte/partials/footer/footer.blade.php ENDPATH**/ ?>
