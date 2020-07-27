ac37ba668809cbe587fca0ecb853d6aa4562e24e
Ahmed Meklad <ahmed.meklad.news@gmail.com>
2020-07-27 13:44:32 +0200

    #comment Modify show invoices (suppliers, sale, raw) by add new barcode  #type MOD #time 00:15 M

--- storage/framework/views/9a9adde9bb2324aa411c5c6d04bac91261cc932c.php
+++
@@ -1,5 +0,0 @@
-<div class="row">
-    <?php echo $records->render(); ?>
-
-</div>
-<?php /**PATH D:\xampp\htdocs\SmartAccountant\vendor\infyomlabs\adminlte-templates\src/../views/common/paginate.blade.php ENDPATH**/ ?>
